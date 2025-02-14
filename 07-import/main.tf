e8288fe2938610be113761d00875e2ac6665147e
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-14 20:51:22 +0530

    terrafrom.server

--- 07-import/main.tf
+++ 07-import/main.tf
@@ -1,4 +1,11 @@
-resource "aws_instance" "workstation" {}
+resource "aws_instance" "workstation" {
+  ami = "i-02fbd2488edd1e856"
+  instance_type = "t3.small"
+  tags
+  = {
+    "Name" = "workstation"
+  }
+}
 
 output "private_ip" {
   value = aws_instance.workstation.private_ip
